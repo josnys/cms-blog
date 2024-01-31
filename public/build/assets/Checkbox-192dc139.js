f9f6f012df12ef9059a8dd1d4be3c9b0b64bc4cc
Josny J. SEVERE <dirjos2014@gmail.com>
2024-01-30 23:03:34 -0500

    Trying Again

---
+++ public/build/assets/Checkbox-192dc139.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-ef9004d9.js";function t({className:o="",...r}){return e.jsx("input",{...r,type:"checkbox",className:"rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 "+o})}export{t as C};
