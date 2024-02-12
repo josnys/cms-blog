790a4453e21b9edfc7241242c626dc262bb51ed4
Josny J. SEVERE <dirjos2014@gmail.com>
2024-02-12 18:51:15 -0500

    Fixed Image Url Bug

--- src/Domains/Media/Actions/UpdateMediaAction.php
+++ src/Domains/Media/Actions/UpdateMediaAction.php
@@ -10,8 +10,10 @@ class UpdateMediaAction
 {
      public function handle(array $data, Media $media): Media
      {
+          $file = $data['url'] ?? $media->url;
+
           $media->name = $data['name'];
-          $media->url = $data['url'];
+          $media->url = $file;
           $media->type = $data['type'];
           $media->mime_type = $data['mime_type'];
           $media->description = $data['description'];
